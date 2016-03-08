eabcfb2d66f498fefa219c2d989e4d870b50f1ad
andy-93 <andreas.ganal@gmx.de>
2016-03-08 10:56:03 +0100

    Andy Test

--- PresentationExample/LeapMotionPPT.py
+++ PresentationExample/LeapMotionPPT.py
@@ -3,6 +3,7 @@ src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
 arch_dir = '../lib/x64' if sys.maxsize > 2**32 else '../lib/x86'
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 import Leap
+# ABC
 from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 from pykeyboard import PyKeyboard
 k=PyKeyboard()
