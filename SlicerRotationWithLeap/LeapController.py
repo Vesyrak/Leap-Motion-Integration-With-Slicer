4496b4d9b753c44ce977041a75ad7a6c7ad77e4f
reinouteyckerman <reinouteyckerman@gmail.com>
2016-05-05 13:41:59 +0000

    Integrated screentap to freeze the screen, Sphere manipulation for zooming and partial implementation of rotation

--- SlicerRotationWithLeap/LeapController.py
+++ SlicerRotationWithLeap/LeapController.py
@@ -5,6 +5,8 @@ from slicer.ScriptedLoadableModule import *
 import logging
 import Leap
 from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
+import time
+
 #
 # LeapController
 #
@@ -84,16 +86,30 @@ class LeapControllerLogic(ScriptedLoadableModuleLogic):
         self.leapbinder=LeapBinder()
         self.controller=Leap.Controller()
         self.listener=SampleListener(self.controller, self.leapbinder)
-
+        self.freerunning=False
         self.slicer=Slicer()
         self.controls=["CircleLeft", "CircleRight"]
+        self.t0=time.time()
     def stop(self):
         print "Stopping Leap Controller Reads"
         self.listener.stop()
+    def stopStart(self, frame, historyframe):
+        t1= time.time()-self.t0
+        if t1>1.0:
+            print t1
+            self.t0=time.time()
+            if not self.freerunning:
+                self.leapbinder.Bind("Zoom", self.slicer.zoom)
+            else:
+                self.leapbinder.Remove("Zoom",self.slicer.zoom)
+            self.freerunning= not self.freerunning
     def bind(self):
-        self.leapbinder.Bind("CircleLeft", self.slicer.rotateLeft)
-        self.leapbinder.Bind("CircleRight", self.slicer.rotateRight)
+        # self.leapbinder.Bind("CircleLeft", self.slicer.rotateLeft)
+        # self.leapbinder.Bind("CircleRight", self.slicer.rotateRight)
+        self.leapbinder.Bind("CircleLeft", self.stopStart)
+        self.leapbinder.Bind("Rotate",self.slicer.rotateWXYZ)
         self.listener.start()
+
 class LeapBinder:
     """Currently used as Event Binder."""
     def __init__(self):
@@ -102,93 +118,70 @@ class LeapBinder:
         if state not in self.bindings:
             self.bindings[state]=[function]
         else: self.bindings[state].append(function)
-    def Remove(self, state, function): #check mistakes
-        if state in function:
+    def Remove(self, state, function):
+        if state in self.bindings:
             if len(self.bindings[state])==1:
                 self.bindings.pop(state)
             else: self.bindings[state].remove(function)
-    def CallFunction(self, state, frame):
-        for i in self.bindings[state]:
-            try:
-                i(frame)
-            except Exception, e:
-                print str(e)
-                print "Exiting due to error"
-                # sys.exit()
+    def CallFunction(self, state, frame, historyframe):
+        if state in self.bindings:
+            print state
+            for i in self.bindings[state]:
+                try:
+                    i(frame, historyframe)
+                except Exception, e:
+                    print str(e)
+                    print "Exiting due to error"
+
 class Slicer:
     def __init__(self):
         self.lm=slicer.app.layoutManager()
-        # self.view=self.lm.threeDWidget(0).threeDView()
-        # self.view.setPitchRollYawIncrement(10)
-        #This is an axis:
-        # self.axis=ctk.ctkAxesWidget()
-        # self.axis.Anterior #This equals to 5, it's an enum
-        #This gets the volume in MRHead
         self.camera=slicer.util.getNode('Default Scene Camera')
         self.transform=slicer.vtkMRMLLinearTransformNode()
-        #This creates a matrix for linear transforms
         slicer.mrmlScene.AddNode(self.transform)
         self.camera.SetAndObserveTransformNodeID(self.transform.GetID())
         self.matrix=vtk.vtkTransform()
         self.degrees=1
-        #Linear transformer. We need to combine this(or an alternative) to the Scalar volume
-#        self.transform.SetAndObserveMatrixTransformToParent()
 
-    # def TransformMatrix():
-        # self.transform.GetMatrixTransformToParent(matrix.GetMatrix())
-        # self.newmatrix.RotateX(20)
-        # vtk.vtkMatrix4x4.Multiply4x4(matrix.GetMatrix(), newmatrix.GetMatrix(), outmatrix.GetMatrix())
-        # self.transform.SetMatrixTransformToParent(outmatrix.GetMatrix())
-
-    def Rotate(direction):
-        if direction == "Left":
-            rotateLeft()
-        elif direction == "Right":
-            rotateRight()
-        elif direction=="Up":
-            rotateUp()
-        elif direction=="Down":
-            rotateDown()
-        elif direction=="CW":
-            rotateCW()
-        elif direction=="CCW":
-            rotateCCW()
-        self.transform.SetMatrixTransformToParent(self.matrix.GetMatrix())
-    def rotateLeft(self, frame): #check axes slicer vs Leap
-
+    def rotateLeft(self, frame, historyframe): #check axes slicer vs Leap
         self.matrix.RotateZ(self.degrees)
         self.transform.SetMatrixTransformToParent(self.matrix.GetMatrix())
-    def rotateRight(self, frame):
+    def rotateRight(self, frame, historyframe):
         self.matrix.RotateZ(-self.degrees)
         self.transform.SetMatrixTransformToParent(self.matrix.GetMatrix())
-    def rotateUp(self, frame):
+    def rotateUp(self, frame, historyframe):
         self.matrix.RotateX(-self.degrees)
-    def rotateDown(self, frame):
+    def rotateDown(self, frame, historyframe):
         self.matrix.RotateX(self.degrees)
-    def rotateCCW(self, frame):
+    def rotateCCW(self, frame, historyframe):
         self.matrix.RotateY(-self.degrees)
-    def rotateCW(self, frame):
+    def rotateCW(self, frame, historyframe):
         self.matrix.RotateY(self.degrees)
-    def zoomOut(self, frame): # link to diameter sphere
-        self.matrix.Translate(0, self.zoom, 0)
-    def zoomIn(self, frame):
-        self.matrix.Translate(0,-self.zoom,0)
-    def moveImg(self, frame): # link to x,y,z of handpalm center average
+    def zoom(self, frame, historyframe): # link to diameter sphere
+        difference= 2*(frame.hands.rightmost.sphere_radius-historyframe.hands.rightmost.sphere_radius)
+        self.matrix.Translate(0,-difference,0)
+        self.transform.SetMatrixTransformToParent(self.matrix.GetMatrix())
+    def moveImg(self, frame, historyframe): # link to x,y,z of handpalm center average
         hand = frame.hands.rightmost
         index_finger_list = hand.fingers.finger_type(Finger.TYPE_INDEX)
         index_finger = index_finger_list[0] #since there is only one per hand
-        bone = finger.bone(Bone.TYPE_DISTAL)
-        basis = bone.basis
-        x = basis.x_basis
-        y = basis.y_basis
-        z = basis.z_basis
+        bonebasis = finger.bone(Bone.TYPE_DISTAL).basis
+        x = bonebasis.x_basis
+        y = bonebasis.y_basis
+        z = bonebasis.z_basis
         self.view.setFocalPoint(x,y,z )
-    def rotateToAxis(self,axis):
+    def rotateToAxis(self,axis, historyframe):
         self.view.lookFromViewAxis(axis)
-
+    def rotateWXYZ(self, frame, historyframe):
+        hm = frame.hands.rightmost.stabilized_palm_position
+        hmo = historyframe.hands.rightmost.stabilized_palm_position
+        print hm.x-hmo.x
+        self.matrix.RotateX((hm.x - hmo.x)*180)
+        self.matrix.RotateZ((hm.y - hmo.y)*180)
+        self.matrix.RotateY((hm.z - hmo.z)*180)
+        self.transform.SetMatrixTransformToParent(self.matrix.GetMatrix())
 class SampleListener(Leap.Listener):
     def __init__(self,controller, leapbinder):
-        print "ARRIBA ARRIBA"
         super(SampleListener, self).__init__()
         self.leapbinder=leapbinder
         self.controller=controller
@@ -196,7 +189,12 @@ class SampleListener(Leap.Listener):
         controller.enable_gesture(Leap.Gesture.TYPE_KEY_TAP);
         controller.enable_gesture(Leap.Gesture.TYPE_SCREEN_TAP);
         controller.enable_gesture(Leap.Gesture.TYPE_SWIPE);
+        controller.config.set("Gesture.ScreenTap.MinForwardVelocity", 20.0)
+        controller.config.set("Gesture.ScreenTap.HistorySeconds", .1)
+        controller.config.set("Gesture.ScreenTap.MinDistance", 2.0)
+        controller.config.save()
         self.run=False
+        self.historyframe=None
     def stop(self):
         self.run=False
     def start(self):
@@ -205,63 +203,41 @@ class SampleListener(Leap.Listener):
     def on_connect(self, controller):
         print "Motion Sensor Connected"
 
-    # def on_disconnect(self, controller):
-        # print "Motion Sensor Disconnected"
-
-    # def on_exit(self, coltroller):
-        # print "Exited"
-
     def on_frame(self):
         # Get the most recent frame and report some basic information
-        print "Frame"
 
         frame = self.controller.frame()
-        print frame
+        if self.historyframe is None:
+            self.historyframe=frame
 
         hands = frame.hands
         for hand in frame.hands:
             handType = "Left Hand" if hand.is_left else "Right Hand"
 
-            # print handType # + " Hand ID:  " + str(hand.id) + " Palm Position:  " + str(hand.palm_position)
-
-            sphere_center = hand.sphere_center
-            sphere_diameter = 2 * hand.sphere_radius
-            pinch = hand.pinch_strength
-            filtered_hand_position = hand.stabilized_palm_position #stabilized palm position of this Hand
-            #print "Diameter: " + str(sphere_diameter) + " Pinch: " + str(pinch) + " Hand pos: " + str(filtered_hand_position)
-
+            if handType=="Right Hand":
+                self.leapbinder.CallFunction("Zoom", frame, self.historyframe)
+                self.leapbinder.CallFunction("Rotate", frame, self.historyframe)
         for gesture in frame.gestures():
             if gesture.type == Leap.Gesture.TYPE_CIRCLE:
-                print "Circle"
                 circle = CircleGesture(gesture)
-                # Circle Gesture
 
                 if circle.pointable.direction.angle_to(circle.normal) <= Leap.PI/2:
                     clockwiseness = "CircleLeft"
                 else:
                     clockwiseness = "CircleRight"
 
-                """swept_angle = 0
-                    if circle.state != Leap.Gesture.STATE_START:
-                        previous = CircleGesture(controller.frame(1).gesture(circle.id))
-                        swept_angle = (circle.progress - previous.progress) * 2 * Leap.PI
-                    print "ID: " + str(CircleGesture.id) + " Progress: " + str(circle.progress) + " Radius: " + str(circle.radius) + " Swept Angle: " + str(swept_angle + Leap.RAD_TO_DEG) + "" + clockwiseness"""
-
                 try:
-                    self.leapbinder.CallFunction(clockwiseness, frame)
+                    self.leapbinder.CallFunction(clockwiseness, frame, self.historyframe)
                 except Exception, e:
                     print str(e)
                     print "Exiting due to error"
-                    sys.exit()
 
             if gesture.type is Leap.Gesture.TYPE_SCREEN_TAP:
-                    screen_tap = ScreenTapGesture(gesture)
+                # screen_tap = ScreenTapGesture(gesture)
+                self.leapbinder.CallFunction("ScreenTap",frame, self.historyframe)
                     # Screen Tap Gesture
                     #print "Screen Tap ID: "+ str(gesture.id) + " State: " + self.state_names[gesture.state] + " Position: " + str(screen_tap.position) + " Directions: " + str(screen_tap.direction)
-                    controller.config.set("Gesture.ScreenTap.MinForwardVelocity", 50.0)
-                    controller.config.set("Gesture.ScreenTap.HistorySeconds", .1)
-                    controller.config.set("Gesture.ScreenTap.MinDistance", 5.0)
-                    controller.config.save()
+        self.historyframe=frame
         if self.run:
             qt.QTimer.singleShot(100, self.on_frame)
 
@@ -285,24 +261,3 @@ class LeapControllerTest(ScriptedLoadableModuleTest):
 
     def test_LeapController1(self):
         pass
-"""def main():
-    # Create a sample listener and controller
-    listener = LeapMotionListener()
-    controller = Leap.Controller()
-
-    # Have the sample listener receive events from the controller
-    controller.add_listener(listener)
-
-    # Keep this process running until Enter is pressed
-    print "Press enter to quit..."
-
-    try:
-        sys.stdin.readline()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        controller.remove_listener(listener)
-        # Remove the sample listener when done
-
-if __name__=="__main__":
-    main()"""
